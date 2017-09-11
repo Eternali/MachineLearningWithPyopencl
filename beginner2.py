23481da2412611e3fc0ff6d21e0e650d060c4484
conrad <conrad.heidebrecht@gmail.com>
2017-09-11 12:40:31 -0400

    update

---
+++ beginner2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import numpy as np
+import pyopencl as cl
+from pyopencl import array
+import matplotlib.pyplot as plt
+import scipy.io as spio
+import time
+import os
+import sys
+
+
